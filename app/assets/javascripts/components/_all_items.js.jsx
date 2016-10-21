e1f99e5a22e8ec2ae6a3cd1d78d5073bee594338
flyingboy007 <abhilashvr007@gmail.com>
2016-10-21 12:38:52 +0530

    added main and body components react

---
+++ app/assets/javascripts/components/_all_items.js.jsx
@@ -0,0 +1,28 @@
+var AllItems = React.createClass({
+    getInitialState: function () {
+        return {items: []}
+    },
+    componentDidMount: function () {
+        console.log('Component mounted');
+        $.getJSON('/api/v1/items.json', (response) => {
+            this.setState({items: response})
+        });
+    },
+    render: function () {
+        var items = this.state.items.map((item) => {
+            return (
+                <div key={item.id}>
+                    <h3>{item.name}</h3>
+                    <p>{item.description}</p>
+                </div>
+            )
+        });
+
+        return (
+            <div>
+                {items}
+            </div>
+        )
+
+    }
+});
---
+++ app/assets/javascripts/components/_body.js.jsx
@@ -0,0 +1,10 @@
+var Body=React.createClass({
+   render: function () {
+       return(
+           <div>
+               <NewItem/>
+               <AllItems/>
+           </div>
+       )
+   }
+});
